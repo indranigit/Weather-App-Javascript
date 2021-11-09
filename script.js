bad0b56cd2c0249d0762ca49bf077bd17653b69c
Indrani Sen <sen.indrani0608@gmail.com>
2021-11-09 16:44:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 let weather = {
-    apiKey: "95aea2fa3086c0c966339a3829c2b016",
+    apiKey: "95aea2fa3086c0c966339a3829c2b016", //public api key
+    
+    //fetchWeather functions grabs the input value which is city and shows the weather of that particular city
     fetchWeather: function (city) {
       fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -10,18 +12,22 @@ let weather = {
       )
         .then((response) => {
           if (!response.ok) {
-            alert("No weather found.");
+            alert("No weather found."); //if the city is not present or value input is invalid the alert is sent and it will throw an error
             throw new Error("No weather found.");
           }
-          return response.json();
+          return response.json();   //else it will return the data in json format
         })
-        .then((data) => this.displayWeather(data));
+        .then((data) => this.displayWeather(data));  //then we will display that data using displayWeather function
     },
+    //here we pass data as parameter and we declare store the name,icon,temp, description etc. as constants. 
+    //There were others as well for example min. temperature, max temperature etc.
     displayWeather: function (data) {
       const { name } = data;
-      const { icon, description } = data.weather[0];
-      const { temp, humidity } = data.main;
+      const { icon, description } = data.weather[0];  //it was present as an array
+      const { temp, humidity } = data.main; // main was containing temperature and humidity as temp and humidity
       const { speed } = data.wind;
+        
+      //here we grab the respective elements using their class naem and change their respective innerText according to the city entered
       document.querySelector(".city").innerText = "Weather in " + name;
       document.querySelector(".icon").src =
         "https://openweathermap.org/img/wn/" + icon + ".png";
@@ -31,19 +37,22 @@ let weather = {
         "Humidity: " + humidity + "%";
       document.querySelector(".wind").innerText =
         "Wind speed: " + speed + " km/h";
-      document.querySelector(".weather").classList.remove("loading");
+        
+      document.querySelector(".weather").classList.remove("loading"); //remove the class "loading" if present. (By default loading class was present
       document.body.style.backgroundImage =
-        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+        "url('https://source.unsplash.com/1600x900/?" + name + "')"; //changing the background image according to the city name entered, images are from unplash
     },
     search: function () {
-      this.fetchWeather(document.querySelector(".search-bar").value);
+      this.fetchWeather(document.querySelector(".search-bar").value);     //search function calls fetchWeather function and passes the value entered as a parameter to city
     },
   };
   
+//adding event listener to the search button when clicked will call the search function
   document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
   });
   
+//similarly, adding event listener to keyboard keys, if enter is pressed the call the search function
   document
     .querySelector(".search-bar")
     .addEventListener("keyup", function (event) {
@@ -52,4 +61,5 @@ let weather = {
       }
     });
   
+//by default we fetch the weather of Ranchi
   weather.fetchWeather("Ranchi");
